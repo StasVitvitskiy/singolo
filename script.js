f8a0fad4a71df59f0ed150d07ecf33bded083746
StasVitvitskiy <stanislav.i.vitvitskiy@gmail.com>
2020-03-12 09:08:18 -0700

    feat: add slider

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const MENU = document.getElementById('menu');
 const CLOSE_BUTTON = document.getElementById('close-btn');
 const FORM = document.getElementById('form');
 const IMG = document.getElementById('portfolio');
-
+const SLIDER = document.getElementById('slider');
 function scrollToElement(pageElement) {
     let positionX = 0,
         positionY = 0;
@@ -50,4 +50,155 @@ CLOSE_BUTTON.addEventListener('click', () => {
     document.getElementById('describe').innerText = "";
     document.getElementById('message-block').classList.add('hidden');
 });
+
+
+
+
+
+let items = document.querySelectorAll('.item');
+let currentItem = 0;
+let isEnabled = true;
+
+function changeCurrentItem(n) {
+    currentItem = (n + items.length) % items.length;
+}
+
+function hideItem(direction) {
+    isEnabled = false;
+    items[currentItem].classList.add(direction);
+    items[currentItem].addEventListener('animationend', function() {
+        this.classList.remove('active', direction);
+    })
+}
+
+function showItem(direction) {
+    items[currentItem].classList.add('next', direction);
+    const dataBackground = items[currentItem].getAttribute('data-background');
+    SLIDER.style.background = dataBackground;
+    items[currentItem].addEventListener('animationend', function() {
+        this.classList.remove('next', direction);
+        this.classList.add('active');
+        isEnabled = true;
+    })
+}
+
+function previousItem(number) {
+    hideItem('to-right');
+    changeCurrentItem(number - 1);
+    showItem('from-left');
+}
+function nextItem(number) {
+    hideItem('to-left');
+    changeCurrentItem(number + 1);
+    showItem('from-right');
+}
+document.querySelector('.left').addEventListener('click', function(){
+    if(isEnabled) {
+        previousItem(currentItem);
+    }
+});
+document.querySelector('.right').addEventListener('click', function(){
+    if(isEnabled) {
+        nextItem(currentItem);
+    }
+});
+
+
+
+const swipeDetect = (el) => {
+
+    let surface = el;
+    let startX = 0;
+    let startY = 0;
+    let distX = 0;
+    let distY = 0;
+
+    let startTime = 0;
+    let elapsedTime = 0;
+
+
+    let threshold = 150;
+    let restraint = 100;
+    let allowedTime = 300;
+
+    surface.addEventListener('mousedown', function(e) {
+        startX = e.pageX;
+        startY = e.pageY;
+        startTime = new Date().getTime();
+        e.preventDefault();
+    });
+
+    surface.addEventListener('mouseup', function(e) {
+        distX = e.pageX - startX;
+        distY = e.pageY - startY;
+        elapsedTime = new Date().getTime() - startTime;
+
+        if(elapsedTime <= allowedTime) {
+            if(Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+                if(distX > 0) {
+                    if(isEnabled) {
+                        previousItem(currentItem);
+                    }
+                } else {
+                    if(isEnabled) {
+                        nextItem(currentItem);
+                    }
+                }
+            }
+        }
+
+
+        e.preventDefault();
+    })
+
+
+    surface.addEventListener('touchstart', function(e) {
+        if(e.target.classList.contains('arrow') || e.target.classList.contains('control')) {
+            if(e.target.classList.contains('left')) {
+                if(isEnabled) {
+                    previousItem(currentItem);
+                }
+            } else if(e.target.classList.contains('right')) {
+                if(isEnabled) {
+                    nextItem(currentItem);
+                }
+            }
+        }
+        let touchObj = e.changedTouches[0];
+        startX = touchObj.pageX;
+        startY = touchObj.pageY;
+        startTime = new Date().getTime();
+        e.preventDefault();
+    });
+
+    surface.addEventListener('touchmove', function(e) {
+        e.preventDefault();
+    });
+
+    surface.addEventListener('touchend', function(e) {
+        let touchObj = e.changedTouches[0];
+        distX = touchObj.pageX - startX;
+        distY = touchObj.pageY - startY;
+        elapsedTime = new Date().getTime() - startTime;
+
+        if(elapsedTime <= allowedTime) {
+            if(Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+                if(distX > 0) {
+                    if(isEnabled) {
+                        previousItem(currentItem);
+                    }
+                } else {
+                    if(isEnabled) {
+                        nextItem(currentItem);
+                    }
+                }
+            }
+        }
+
+
+        e.preventDefault();
+    })
+}
+let el = document.querySelector('.slider');
+swipeDetect(el);
 
